accfb1a575e837be4316fb6d57a773ae4130d617
artichokeee <rusau.qalife@gmail.com>
2023-10-27 01:23:03 +0300

    updated lesson_2.3.1.1.js with 2.3.1.2. answer

--- lesson_2.3.1.1.js
+++ lesson_2.3.1.1.js
@@ -30,3 +30,68 @@ for (elementKey in players) {
 console.log(scorePoints);
 
 console.log(`The max score point is ${Math.max(...scorePoints)}`);
+
+// задание 2.3.1.2
+
+const newPlayers = [
+    {
+      id: 5,
+      name: "Elena",
+      scorePoints: 2800,
+    },
+    {
+      id: 6,
+      name: "Alex",
+      scorePoints: 4100,
+    },
+    {
+      id: 7,
+      name: "Sasha",
+      scorePoints: 3200,
+    },
+    {
+      id: 8,
+      name: "Nina",
+      scorePoints: 3900,
+    },
+    {
+      id: 9,
+      name: "Dmitri",
+      scorePoints: 5000,
+    },
+    {
+      id: 10,
+      name: "Lena",
+      scorePoints: 2750,
+    },
+    {
+      id: 11,
+      name: "Andrey",
+      scorePoints: 3800,
+    },
+    {
+      id: 12,
+      name: "Maria",
+      scorePoints: 2950,
+    },
+    {
+      id: 13,
+      name: "Anton",
+      scorePoints: 3600,
+    },
+    {
+      id: 14,
+      name: "Svetlana",
+      scorePoints: 2700,
+    }
+  ];
+  
+  let newScorePoints = [];
+  
+  for (elementKey in newPlayers) {
+    newScorePoints.push(newPlayers[elementKey].scorePoints);
+  }
+  
+  console.log(newScorePoints);
+  
+  console.log(`The max score point is ${Math.max(...newScorePoints)}`);
